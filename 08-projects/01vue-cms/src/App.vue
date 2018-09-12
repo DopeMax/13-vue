6f64e083101538fb2a4f70c39056a5b35747ed07
max.chen <amingccc@gmail.com>
2018-09-12 15:52:07 +0800

    home轮播，九宫格，页动画效果

--- 08-projects/01vue-cms/src/App.vue
+++ 08-projects/01vue-cms/src/App.vue
@@ -5,28 +5,30 @@
 			
 		</mt-header>
 		<!-- 中间的路由router-view区域 -->
+		<transition>
+			<router-view></router-view>
+		</transition>
 
 		<!-- 底部tabbar区域 -->
 		<nav class="mui-bar mui-bar-tab">
-			<a class="mui-tab-item mui-active" href="#tabbar">
+			<router-link class="mui-tab-item" to="/home">
 				<span class="mui-icon mui-icon-home"></span>
 				<span class="mui-tab-label">首页</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-chat">
+			</router-link>
+			<router-link class="mui-tab-item" to="/member">
 				<span class="mui-icon mui-icon-contact"></span>
 				<span class="mui-tab-label">会员</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-contact">
-				<span class="mui-icon mui-icon-contact"><span class="mui-badge">9</span></span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/shoppingcart">
+				<span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">9</span></span>
 				<span class="mui-tab-label">购物车</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-map">
+			</router-link>
+			<router-link class="mui-tab-item" to="/search">
 				<span class="mui-icon mui-icon-search"></span>
 				<span class="mui-tab-label">搜索</span>
-			</a>
+			</router-link>
 		</nav>
 
-		<h1>这是APP组件</h1>
 	</div>
 </template>
 
@@ -37,5 +39,17 @@
 <style lang="scss" scoped>
 .app-container {
   padding-top: 40px;
+  overflow-x: hidden;
+}
+
+.v-enter,
+.v-leave-to {
+  opacity: 0;
+  transform: translateX(100%);
+}
+.v-enter-active,
+.v-leave-to-active {
+  opacity: 0;
+  transition: all 0.5s ease;
 }
 </style>
