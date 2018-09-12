f9c6cac447ed410a4bcfba4da7a0ff989ed55c1c
max.chen <amingccc@gmail.com>
2018-09-12 13:06:39 +0800

    header和tabbar

--- 08-projects/01vue-cms/src/App.vue
+++ 08-projects/01vue-cms/src/App.vue
@@ -1,5 +1,31 @@
 <template>
-	<div>
+	<div class="app-container">
+		<!-- 顶部 Header区域-->
+		<mt-header fixed title="黑马程序员-vue项目">
+			
+		</mt-header>
+		<!-- 中间的路由router-view区域 -->
+
+		<!-- 底部tabbar区域 -->
+		<nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">消息</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">通讯录</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">设置</span>
+			</a>
+		</nav>
+
 		<h1>这是APP组件</h1>
 	</div>
 </template>
@@ -9,5 +35,7 @@
 
 
 <style lang="scss" scoped>
-
+.app-container {
+  padding-top: 40px;
+}
 </style>
